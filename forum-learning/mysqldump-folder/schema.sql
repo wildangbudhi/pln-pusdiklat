5e7f6cf08b6bfede945b486dc108d263b77ed8c0
wildangbudhi <wildagb2@gmail.com>
2021-01-05 19:42:29 +0700

    Update SQL Schema for Discussion Expertise

--- forum-learning/mysqldump-folder/schema.sql
+++ forum-learning/mysqldump-folder/schema.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
 --
--- Host: localhost    Database: forum_learning
+-- Host: 127.0.0.1    Database: forum_learning
 -- ------------------------------------------------------
--- Server version	5.7.32-0ubuntu0.18.04.1
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `activities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activities` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `module_name` varchar(100) DEFAULT NULL,
   `method` varchar(4) DEFAULT NULL,
   `url` varchar(100) DEFAULT NULL,
@@ -51,7 +51,7 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `category_name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -74,8 +74,8 @@ DROP TABLE IF EXISTS `discussion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `forum_id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `forum_id` bigint NOT NULL,
   `title` varchar(100) DEFAULT NULL,
   `zoom_invitation_url` varchar(100) DEFAULT NULL,
   `zoom_host_email` varchar(100) DEFAULT NULL,
@@ -84,9 +84,9 @@ CREATE TABLE `discussion` (
   `end_datetime` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `discussion_request_id` bigint(20) DEFAULT NULL,
+  `discussion_request_id` bigint DEFAULT NULL,
   `status` varchar(100) DEFAULT NULL,
-  `category_id` bigint(20) DEFAULT NULL,
+  `category_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `discussion_FK_forum_id` (`forum_id`),
   KEY `discussion_FK` (`discussion_request_id`),
@@ -114,14 +114,14 @@ DROP TABLE IF EXISTS `discussion_expertises`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_expertises` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `discussion_id` bigint(20) DEFAULT NULL,
+  `expertise_id` bigint DEFAULT NULL,
+  `discussion_id` bigint DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  KEY `discussion_expertises_FK_user_id` (`user_id`),
+  KEY `discussion_expertises_FK_user_id` (`expertise_id`),
   KEY `discussion_expertises_FK` (`discussion_id`),
   CONSTRAINT `discussion_expertises_FK` FOREIGN KEY (`discussion_id`) REFERENCES `discussion` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `discussion_expertises_FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_auth` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `discussion_expertises_FK_expertise_id` FOREIGN KEY (`expertise_id`) REFERENCES `expertises` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -142,8 +142,8 @@ DROP TABLE IF EXISTS `discussion_participants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_participants` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `discussion_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `discussion_id` bigint DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   KEY `discussion_participants_FK_user_id` (`user_id`),
@@ -170,8 +170,8 @@ DROP TABLE IF EXISTS `discussion_participants_waiting_list`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_participants_waiting_list` (
-  `discussion_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
+  `discussion_id` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
   `accepted` tinyint(1) DEFAULT NULL,
   `accapted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -200,8 +200,8 @@ DROP TABLE IF EXISTS `discussion_reaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_reaction` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `discussion` bigint(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `discussion` bigint DEFAULT NULL,
   `up_vote` tinyint(1) DEFAULT NULL,
   `down_vote` tinyint(1) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -230,12 +230,12 @@ DROP TABLE IF EXISTS `discussion_request_expertise_sugestions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_request_expertise_sugestions` (
-  `discussion_request_id` bigint(20) DEFAULT NULL,
-  `expertise_user_id` bigint(20) DEFAULT NULL,
-  KEY `discussion_request_expertise_sugestions_FK_1` (`expertise_user_id`),
+  `discussion_request_id` bigint DEFAULT NULL,
+  `expertise_id` bigint DEFAULT NULL,
+  KEY `discussion_request_expertise_sugestions_FK_1` (`expertise_id`),
   KEY `discussion_request_expertise_sugestions_FK` (`discussion_request_id`),
   CONSTRAINT `discussion_request_expertise_sugestions_FK` FOREIGN KEY (`discussion_request_id`) REFERENCES `discussion_requests` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `discussion_request_expertise_sugestions_FK_1` FOREIGN KEY (`expertise_user_id`) REFERENCES `user_auth` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `discussion_request_expertise_sugestions_FK_expertise_id` FOREIGN KEY (`expertise_id`) REFERENCES `expertises` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -256,13 +256,13 @@ DROP TABLE IF EXISTS `discussion_requests`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_requests` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `forum_id` bigint(20) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `forum_id` bigint DEFAULT NULL,
   `accepted` tinyint(1) DEFAULT NULL,
   `accepted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `reqeuster_user_id` bigint(20) DEFAULT NULL,
+  `reqeuster_user_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `discussion_requests_FK` (`forum_id`),
   KEY `discussion_requests_FK_1` (`reqeuster_user_id`),
@@ -288,7 +288,7 @@ DROP TABLE IF EXISTS `discussion_requests_start_datetime_sugestions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discussion_requests_start_datetime_sugestions` (
-  `discussion_request_id` bigint(20) DEFAULT NULL,
+  `discussion_request_id` bigint DEFAULT NULL,
   `start_datetime` timestamp NULL DEFAULT NULL,
   KEY `discussion_requests_start_datetime_sugestions_FK` (`discussion_request_id`),
   CONSTRAINT `discussion_requests_start_datetime_sugestions_FK` FOREIGN KEY (`discussion_request_id`) REFERENCES `discussion_requests` (`id`) ON UPDATE CASCADE
@@ -305,6 +305,40 @@ LOCK TABLES `discussion_requests_start_datetime_sugestions` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `expertises`
+--
+
+DROP TABLE IF EXISTS `expertises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expertises` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `identity_id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `tempat_tanggal_lahir` varchar(200) DEFAULT NULL,
+  `telp_number` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `category_id` bigint DEFAULT NULL,
+  `pendidikan_terakhir` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` varchar(20) DEFAULT NULL,
+  `rekening_number` varchar(100) DEFAULT NULL,
+  `rekening_owner_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `expertises_FK_category_id` (`category_id`),
+  CONSTRAINT `expertises_FK_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expertises`
+--
+
+LOCK TABLES `expertises` WRITE;
+/*!40000 ALTER TABLE `expertises` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expertises` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `forum`
 --
 
@@ -312,10 +346,10 @@ DROP TABLE IF EXISTS `forum`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `forum` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
-  `author_user_id` bigint(20) NOT NULL,
-  `category_id` bigint(20) NOT NULL,
+  `author_user_id` bigint NOT NULL,
+  `category_id` bigint NOT NULL,
   `status` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -344,8 +378,8 @@ DROP TABLE IF EXISTS `forum_reaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `forum_reaction` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `forum_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `forum_id` bigint DEFAULT NULL,
   `up_vote` tinyint(1) DEFAULT NULL,
   `down_vote` tinyint(1) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -374,9 +408,9 @@ DROP TABLE IF EXISTS `forum_replies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `forum_replies` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `forum_id` bigint(20) NOT NULL,
-  `author_user_id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `forum_id` bigint NOT NULL,
+  `author_user_id` bigint NOT NULL,
   `answer` varchar(10000) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -405,9 +439,9 @@ DROP TABLE IF EXISTS `forum_replies_reactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `forum_replies_reactions` (
-  `user_id` bigint(20) NOT NULL,
-  `forum_id` bigint(20) NOT NULL,
-  `forum_replies_id` bigint(20) NOT NULL,
+  `user_id` bigint NOT NULL,
+  `forum_id` bigint NOT NULL,
+  `forum_replies_id` bigint NOT NULL,
   `up_vote` tinyint(1) DEFAULT NULL,
   `down_vote` tinyint(1) DEFAULT NULL,
   `agree` tinyint(1) DEFAULT NULL,
@@ -440,11 +474,11 @@ DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `title` varchar(100) DEFAULT NULL,
-  `discussion_id` bigint(20) DEFAULT NULL,
-  `author_user_id` bigint(20) DEFAULT NULL,
-  `category_id` bigint(20) DEFAULT NULL,
+  `discussion_id` bigint DEFAULT NULL,
+  `author_user_id` bigint DEFAULT NULL,
+  `category_id` bigint DEFAULT NULL,
   `articles` text,
   `thumbnail` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -476,8 +510,8 @@ DROP TABLE IF EXISTS `posts_reactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts_reactions` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `posts_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `posts_id` bigint DEFAULT NULL,
   `up_vote` tinyint(1) DEFAULT NULL,
   `down_vote` tinyint(1) DEFAULT NULL,
   `agree` tinyint(1) DEFAULT NULL,
@@ -508,8 +542,8 @@ DROP TABLE IF EXISTS `role_activities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role_activities` (
-  `role_id` bigint(20) DEFAULT NULL,
-  `activities_id` bigint(20) DEFAULT NULL,
+  `role_id` bigint DEFAULT NULL,
+  `activities_id` bigint DEFAULT NULL,
   KEY `role_activities_FK` (`role_id`),
   KEY `role_activities_FK_1` (`activities_id`),
   CONSTRAINT `role_activities_FK` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE,
@@ -534,7 +568,7 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `roles_name` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -559,15 +593,15 @@ DROP TABLE IF EXISTS `user_auth`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_auth` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `full_name` varchar(255) DEFAULT NULL,
   `avatar_file` varchar(64) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `status_id` tinyint(4) DEFAULT NULL,
-  `user_type_id` tinyint(4) DEFAULT NULL,
-  `user_entity_id` bigint(20) DEFAULT NULL,
+  `status_id` tinyint DEFAULT NULL,
+  `user_type_id` tinyint DEFAULT NULL,
+  `user_entity_id` bigint DEFAULT NULL,
   `user_key` varchar(64) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -596,8 +630,8 @@ DROP TABLE IF EXISTS `user_roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_roles` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `role_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `role_id` bigint DEFAULT NULL,
   KEY `user_roles_FK` (`user_id`),
   KEY `user_roles_FK_1` (`role_id`),
   CONSTRAINT `user_roles_FK` FOREIGN KEY (`user_id`) REFERENCES `user_auth` (`id`) ON UPDATE CASCADE,
@@ -623,4 +657,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-17 11:10:20
+-- Dump completed on 2021-01-05 19:41:48
