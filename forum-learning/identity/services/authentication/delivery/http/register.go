f4b5c026cf6afaa089fc1b445761083f11e1a7ab
wildangbudhi <wildagb2@gmail.com>
2021-01-06 20:38:44 +0700

    [Identity] Done Register, Authenticate, Verify Function

---
+++ forum-learning/identity/services/authentication/delivery/http/register.go
@@ -0,0 +1,64 @@
+package http
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wildangbudhi/pln-pusdiklat/forum-learning/identity/services/authentication/domain"
+)
+
+type registerRequestBody struct {
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type registerResponseBody struct {
+	UserAuthID int64 `json:"user_auth_id" binding:"required"`
+}
+
+// Register is a HTTP Handler for Register Usecase
+func (handler *AuthenticationHTTPHandler) Register(c *gin.Context) {
+
+	c.Header("Content-Type", "application/json")
+
+	requestBodyData := &registerRequestBody{}
+
+	c.BindJSON(requestBodyData)
+
+	if requestBodyData.Email == "" {
+		c.JSON(http.StatusBadRequest, domain.HTTPErrorResponse{Error: "Email tidak boleh kosong"})
+		return
+	}
+
+	if requestBodyData.Username == "" {
+		c.JSON(http.StatusBadRequest, domain.HTTPErrorResponse{Error: "Username tidak boleh kosong"})
+		return
+	}
+
+	if requestBodyData.Password == "" {
+		c.JSON(http.StatusBadRequest, domain.HTTPErrorResponse{Error: "Password tidak boleh kosong"})
+		return
+	}
+
+	requestBodyData.Email = strings.ToLower(requestBodyData.Email)
+
+	email, err := domain.NewEmail(requestBodyData.Email)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.HTTPErrorResponse{Error: err.Error()})
+		return
+	}
+
+	userAuthID, err := handler.authenticationUsecase.Register(requestBodyData.FullName, *email, requestBodyData.Username, requestBodyData.Password)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.HTTPErrorResponse{Error: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, registerResponseBody{UserAuthID: userAuthID})
+
+}
---
+++ forum-learning/identity/services/authentication/domain/model/user_auth.go
@@ -0,0 +1,27 @@
+package model
+
+import "database/sql"
+
+// Roles is Struct Model for roles table in database
+type Roles struct {
+	ID       int    `json:"id"`
+	RoleName string `json:"role_name"`
+}
+
+// UserAuth is Struct Model for user_auth table in database
+type UserAuth struct {
+	ID       int            `json:"id"`
+	FullName sql.NullString `json:"full_name"`
+	Email    string         `json:"email"`
+	Username string         `json:"username"`
+	Password string         `json:"password"`
+	Roles    []Roles        `json:"roles"`
+}
+
+// UserAuthRepository Interface is a contract of Repository for User Auth Table
+type UserAuthRepository interface {
+	GetUserAuthByID(id int) (*UserAuth, error)
+	GetUserAuthByEmail(email string) (*UserAuth, error)
+	GetUserAuthByUsername(username string) (*UserAuth, error)
+	InsertUserAuth(userAuth *UserAuth) (int64, error)
+}
