c363e8fa8b006bfa2af9b3597b1734fdd4f63707
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2021-02-12 09:23:59 +0700

    Migrate FrontEnd Container to native Python Image

--- forum-learning/front-end_admin/app/main.py
+++ forum-learning/front-end_admin/app/main.py
@@ -6,9 +6,7 @@ from posts.routes import posts
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.register_blueprint(homepage)
-    app.register_blueprint(qna)
-    app.register_blueprint(discussions)
-    app.register_blueprint(posts)
-    app.run(host="0.0.0.0",port=80, debug=True)
+app.register_blueprint(homepage)
+app.register_blueprint(qna)
+app.register_blueprint(discussions)
+app.register_blueprint(posts)
