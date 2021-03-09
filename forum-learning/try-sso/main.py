f55ff6ee59224afd3aa23d942f0e92fd2bb53b0e
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2021-03-09 12:26:33 +0700

    Update EntityID

--- forum-learning/try-sso/main.py
+++ forum-learning/try-sso/main.py
@@ -32,7 +32,7 @@ app.config['SAML2_IDENTITY_PROVIDERS'] = [
         'CLASS': 'flask_saml2.sp.idphandler.IdPHandler',
         'OPTIONS': {
             'display_name': "pln-pusdiklat",
-            'entity_id': 'https://idp.pln-pusdiklat.co.id',
+            'entity_id': 'forumlearning.pln.co.id',
             'sso_url': 'https://elearning.pln-pusdiklat.co.id/sso/saml2/idp/SSOService.php',
             'slo_url': 'https://elearning.pln-pusdiklat.co.id/sso/saml2/idp/SingleLogoutService.php',
             'certificate': IDP_CERTIFICATE,
